96e34a1b6066871bd27f8331d46f1b3289514576
RJ <rajeev15feb@gmail.com>
2025-02-13 16:56:30 +0530

    add test with scenario single length

--- test.py
+++ test.py
@@ -5,4 +5,10 @@ def test_add_blank_string():
     expected = 0
     total = add(input_str)
     assert expected == total
+
+def test_add_single_string():
+    input_str = "1"
+    expected = 1
+    total = add(input_str)
+    assert expected == total
     
